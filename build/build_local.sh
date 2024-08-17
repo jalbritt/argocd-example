098089c70406ab1b5553314381a299d57e218edf
James Albritton <jalbritt@users.noreply.github.com>
2024-08-17 16:04:58 -0500

    update

--- build/build_local.sh
+++ build/build_local.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Start Minikube
-minikube start
+if [ ! $(minikube status | grep "host: Running" | wc -l) -eq 1 ]; then
+  minikube start
+fi
 
 # Install ArgoCD
 kubectl create namespace argocd
@@ -14,9 +15,8 @@ echo "Waiting for ArgoCD server to be ready..."
 kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
 kubectl wait --for=condition=available --timeout=300s deployment/argocd-repo-server -n argocd
 
-# Port forward ArgoCD API server
+# Port forward ArgoCD API server. Ampersand ensures script continues.
 kubectl port-forward svc/argocd-server -n argocd 8080:443 &
-sleep 5
 
 # Get the ArgoCD admin password
 ARGO_PWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
@@ -25,23 +25,27 @@ ARGO_PWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath=
 argocd login localhost:8080 --username admin --password $ARGO_PWD --insecure
 
 # Create the argo app
-argocd app create argocd-app \
-   --repo https://github.com/jalbritt/argocd-example \
-   --path build/argo-resources \
-   --dest-server https://kubernetes.default.svc \
-   --dest-namespace argocd \
-   --sync-policy automated
+kubectl apply -f build/argo-resources/argocd-app.yaml
 
-sleep 10
+#argocd app create argocd-app \
+#   --repo https://github.com/jalbritt/argocd-example \
+#   --path build/argo-resources \
+#   --dest-server https://kubernetes.default.svc \
+#   --dest-namespace argocd \
+#   --sync-policy automated
+
 # Create the standalone app
-argocd app create standalone-app \
-    --repo https://github.com/jalbritt/argocd-example \
-    --path demos/standalone-app/hello-world \
-    --dest-server https://kubernetes.default.svc \
-    --dest-namespace argocd \
-    --sync-policy automated
+kubectl apply -f demos/standalone-app/standalone-app.yaml
+
+#argocd app create standalone-app \
+#    --repo https://github.com/jalbritt/argocd-example \
+#    --path demos/standalone-app/hello-world \
+#    --dest-server https://kubernetes.default.svc \
+#    --dest-namespace argocd \
+#    --sync-policy automated
 
 # Create the app-of-apps
+# kubectl apply -f demos/app-of-apps/app-of-apps.yaml
 argocd app create app-of-apps \
     --repo https://github.com/jalbritt/argocd-example \
     --path demos/app-of-apps \
